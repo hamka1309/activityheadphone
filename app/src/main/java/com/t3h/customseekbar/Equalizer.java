37459531f0898facf0e17de70772a0fceaf53be9
hadtt <hadtt@segu.vn>
2019-08-20 19:00:32 +0700

    w

---
+++ app/src/main/java/com/t3h/customseekbar/Equalizer.java
@@ -0,0 +1,27 @@
+package com.t3h.customseekbar;
+
+public class Equalizer {
+    private String image;
+    private String title;
+
+    public Equalizer(String image, String title) {
+        this.image = image;
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- app/src/main/java/com/t3h/customseekbar/MainActivity.java
+++ app/src/main/java/com/t3h/customseekbar/MainActivity.java
@@ -1,23 +1,52 @@
 package com.t3h.customseekbar;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    String[] equalizer={"Flat","Bass Only","Treble Only","Rock","Grunge","Metal","Dance","Country","Jazz","Speech","Classical","Blues","Opera","Swing","Acoustic","New Age"};
+    ListView listView;
+    TextView textView;
+    String[] listItem;
+    private Dialog dialog;
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.headphone_activity);
-        toolbar = findViewById(R.id.toobal1);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.profile_ic_arrow_left);
+//        toolbar = findViewById(R.id.toobal1);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.profile_ic_arrow_left);
+
+
+        listView=(ListView)findViewById(R.id.listView);
+        textView=(TextView)findViewById(R.id.textView);
+        listItem = getResources().getStringArray(R.array.array_technology);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
+        listView.setAdapter(adapter);
     }
+    public void showDialog() {
+        dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Thangcode.com");
+        dialog.setContentView(R.layout.activity_main);
+        dialog.show();
+    }
+
+
 }
